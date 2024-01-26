9bb644a16d247952fd4e5d3c3f956f431d061e5d
tasrif <tasrif701@gmail.com>
2024-01-26 16:55:58 +0600

    game starting, speedscale, score updating added

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ import { setupGround, updateGround } from "./ground.js"
 
   const WORLD_WIDTH = 100
   const WORLD_HEIGHT = 30
+  const SPEED_SCALE_INCREASE = 0.0001
 
   const worldElem = document.querySelector("[data-world]")
-
+  const scoreElem = document.querySelector("[data-score]")
+  const startScreenElem = document.querySelector("[data-start-screen]")
+   
 
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
-
-setupGround()
+document.addEventListener("keydown", handleStart, { once: true })  // adding the press to start feature
 
 // update loop will run every frame of the elements in game
 let lastTime
+let speedScale
+let score
 function update(time) {
     if (lastTime == null) {
         lastTime = time
@@ -20,15 +24,38 @@ function update(time) {
         return
     }
 
-    const delta = time -lastTime  // delta is time between frames
+    const delta = time - lastTime  // delta is time between frames
     //console.log(delta)
 
-    updateGround(delta, 1)
+    updateGround(delta, speedScale)
+    updateSpeedScale(delta)
+    updateScore(delta)
     
     lastTime = time
     window.requestAnimationFrame(update)
 }
-window.requestAnimationFrame(update)
+
+
+function updateSpeedScale(delta) {
+    speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta) {
+    score += delta * 0.01
+    scoreElem.textContent = Math.floor(score)
+}
+
+//press to start feature and go on
+function handleStart() {
+    lastTime = null
+    speedScale = 1;
+    score = 0;
+
+    setupGround()
+    startScreenElem.classList.add("hide")
+    window.requestAnimationFrame(update)
+}
+
 
 //   scaling the game wworld according to the window
 function setPixelToWorldScale() {
