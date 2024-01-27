d3ab06f117173daf2c87857a92af9ebcaeceaf38
tasrif <tasrif701@gmail.com>
2024-01-27 15:43:01 +0600

    dino jumping and event listener added

--- dino.js
+++ dino.js
@@ -1,27 +1,35 @@
+import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./updatCustomProperty.js"
+
 const dinoElem = document.querySelector("[data-dino]")
 const JUMP_SPEED = .45
-const GRAVITY = .011
+const GRAVITY = .0015
 const DINO_FRAME_COUNT = 2
 const FRAME_TIME = 100
 
 let isJumping
 let dinoFrame
-let currentFrameTime   
+let currentFrameTime 
+let yVelocity  
 
 export function setupDino() {
      isJumping = false
      dinoFrame = 0
      currentFrameTime = 0
+     yVelocity = 0  
+     setCustomProperty(dinoElem, "--bottom", 0)
+
+     document.removeEventListener("keydown", onJump)
+     document.addEventListener("keydown", onJump)
 }
 
 export function updateDino(delta, speedScale) {
     handleRun(delta, speedScale)
-    handleJump()
+    handleJump(delta)
 }
 
 function handleRun(delta, speedScale) {
     if (isJumping) {
-        dinoElem.src = `images/dino-stationary.jpg`
+        dinoElem.src = `images/dino-stationary.png`
         return;     
     }
 
@@ -34,6 +42,25 @@ function handleRun(delta, speedScale) {
     currentFrameTime += delta * speedScale
 }
 
-function handleJump() {
+function handleJump(delta) {
+    if(!isJumping) return
 
+    // moving the dino body upwards    
+    incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta)
+    
+    // condition for the dino body to land on ground
+    if(getCustomProperty(dinoElem, "--bottom") <= 0) {
+        setCustomProperty(dinoElem, "--bottom", 0)
+        isJumping = false
+    }
+    
+    yVelocity -= GRAVITY * delta
+}
+
+//event listener for jumping
+function onJump(e) {
+    if (e.code !== "Space" || isJumping) return
+
+    yVelocity = JUMP_SPEED
+    isJumping = true
 }
